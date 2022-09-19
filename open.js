1c2601c815a83f73ff2639822afe3560ed317449
John-Baird <106564524+John-Baird@users.noreply.github.com>
2022-09-19 09:50:59 -0600

    Added deletion to lists

--- open.js
+++ open.js
@@ -1,7 +1,57 @@
 
 
-function post(){
-    a = document.getElementsByTagName("input")[0].value
-    $('.here').append('<div class="item"><input type="checkbox" class="box"><div class="word">'+a+'</div></div>')
+let todo = []
+let todoShow = []
+$(document).ready(function () {
 
-}
+    $('.roww').on('click', 'button', function(){
+        a = document.getElementsByTagName("input")[0].value;
+        
+            if (a !== ""){
+                todo.push(a)
+                console.log(todo)
+                todoAdd();
+                
+            }
+        function todoAdd(){
+            for (let i = 0; i < todo.length; i++){
+                
+                todoShow.splice(i,1,'<div class="item"><input type="checkbox" class="box"><div class="word">'+todo[i]+'</div><div class="delete">🗑</div><div class="edit">✏️</div></div>')
+                console.log(todoShow)
+                
+            }
+            b = todoShow.length
+            b = b-1;
+            $('.here').append(todoShow[b])
+        }
+        
+    });
+
+
+    $('div').on('click','.delete', function(){
+        d = this.parentElement
+        console.log(d)
+        console.log(d.innerText[0])
+        e = d.innerText[0]
+        todo.forEach(removeList)
+        function removeList(item){
+            if (item == e){
+                
+                f = todo.indexOf(item)
+                todo.splice(f,1)
+                todoShow.splice(f,1)
+                console.log(todo)
+                console.log(todoShow)
+            }
+        }
+        d.remove()
+        
+    })
+
+
+
+});
+
+
+
+
